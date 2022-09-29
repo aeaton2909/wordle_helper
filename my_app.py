e8caa2a4c924e46e201f3a53ab25f7a929a2a316
Andrew Eaton <ae7787@hotmail.com>
2022-09-29 17:07:37 +0100

    Cosmetic changes

--- my_app.py
+++ my_app.py
@@ -4,8 +4,12 @@ from play_wordle import PlayWorldle
 import pandas as pd
 # Write a page title
 st.title('Wordle Helper')
+para = ()
 st.text(
-    'This app uses information theory to help you solve Wordle!\nType in the guessed word and the letters that are green or amber (blank if none).\nHint: "alert" is a great guess to start with..'
+    """This app uses information theory to help you solve Wordle!
+Tell it your guessed word and which letters are green or amber (leave blank if none).
+The app tells you which word to try next.
+Hint: "alert" is a great guess to start with.."""
     )
 
 if 'game_object' not in st.session_state:
@@ -15,17 +19,16 @@ if 'game_object' not in st.session_state:
 
     # Intialise object with words
     st.session_state['game_object'] = PlayWorldle(wordle_words)
-    
+
 guessed_word = st.text_input('Word you guessed:', 'alert')
 green_chars = st.text_input('Letters that are GREEN:', )
 amber_chars = st.text_input('Letters that are AMBER:', )
-
 push_button = st.button('Enter')
 
 if push_button:
-    st.session_state['game_object'].enter_guess(guessed_word, 
-                                                green_chars, 
-                                                amber_chars)
+    st.session_state['game_object'].enter_guess(guessed_word.lower(), 
+                                                green_chars.lower(), 
+                                                amber_chars.lower())
     best_choices = st.session_state['game_object'].gen_word_entropy(return_best=True)
 
     n_remaining = len(st.session_state['game_object'].remaining_words)
@@ -33,14 +36,11 @@ if push_button:
     if n_remaining == 1:
         remaining_output = ('CONGRATULATIONS! The only word left is "{}"!'
                             .format(st.session_state['game_object'].remaining_words[0]))
+        chart_data = pd.Series(st.session_state['game_object'].remaining_words_log)
+        st.bar_chart(chart_data.rename(index='Remaining words'))
     elif n_remaining == 0:
         remaining_output = ('There are no words left...something went wrong!')
     else:
         remaining_output = 'There are {} remaining words!'.format(n_remaining)
         st.write('Try this word:       ', '"{}"'.format(best_choices[0]))
     st.write('', remaining_output)
-
-
-    chart_data = pd.Series(st.session_state['game_object'].remaining_words_log)
-    # st.line_chart(chart_data.rename(index='Remaining words'))
-    st.bar_chart(chart_data.rename(index='Remaining words'))
